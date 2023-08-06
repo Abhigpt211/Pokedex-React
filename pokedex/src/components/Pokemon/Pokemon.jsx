fe8c26df8365548111856e5515a8c32833ac6091
Abhigpt211 <abhigpt211@gmail.com>
2023-08-06 23:52:06 +0530

    This is React Pokedex

---
+++ pokedex/src/components/Pokemon/Pokemon.jsx
@@ -0,0 +1,14 @@
+import  './Pokemon.css'
+
+function Pokemon({name, image}) {
+   return(
+     <div className='pokemon'>
+        <div className='pokemon-name'>{name}</div>
+        <div>
+            <img className='pokemon-image' src={image} />
+        </div>
+     </div>
+   )
+}
+
+export default Pokemon;
